9b9392174152e85121201fbbb408032dc95a2b2d
Matloga Tsumbedzo <tsumbedzomatloga@gmail.com>
2025-10-17 16:06:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9135278969-63035",
+  "appId": "1:894203867873:web:f484394661e263e9317da0",
+  "apiKey": "AIzaSyDwedzbIuOyKthJUg850F_rT4BUUduobwM",
+  "authDomain": "studio-9135278969-63035.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "894203867873"
+};
