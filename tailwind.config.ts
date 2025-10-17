473a69703fe8e8b7996caede1d93ecad229c55c1
Matloga Tsumbedzo <tsumbedzomatloga@gmail.com>
2025-10-17 08:31:56 +0000

    Instead of falling they can just loop in the form of rounding

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,11 +91,16 @@ export default {
             transform: 'scale(1.1)',
           },
         },
+        'scroll': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'pulse-slow': 'pulse-slow 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'scroll': 'scroll 20s linear infinite',
       },
     },
   },
